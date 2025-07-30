0a44172fc7f5500fdf15d53815ae3047063dcd84
joshua <joshuanwosu078@gmail.com>
2025-07-30 20:08:21 +0100

    updates

--- src/lib/router.tsx
+++ src/lib/router.tsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter } from 'react-router-dom';
-import XMLXSDConverter from '../components/xml-xsd-converter';
-import RootLayout from '../layout/root-layout';
+import XMLXSDConverter from '@/components/xml-xsd-converter';
+import RootLayout from '@/layout/root-layout';
+import HomePage from '@/pages/home';
+import NotFoundPage from '@/pages/not-found';
 
 export const router = createBrowserRouter([
   {
@@ -8,7 +10,15 @@ export const router = createBrowserRouter([
     children: [
       {
         path: '/',
+        element: <HomePage />,
+      },
+      {
+        path: '/xml-editor',
         element: <XMLXSDConverter />,
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
       },
     ],
   },
